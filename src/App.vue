dba518896b27f1179fd5b60467da7cf84213844c
Leo01234 <40910694+Leo01234@users.noreply.github.com>
2024-04-06 19:44:11 +0800

    add celebration animation

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import BirthdayCake from './components/BirthdayCake.vue'
+import CelebrationAnimation from './components/CelebrationAnimation.vue'
 </script>
 
 <template>
   <BirthdayCake />
+  <CelebrationAnimation />
 </template>
 
 <style scoped>
